3779336408687a17fb75849516f40b256ddb8561
peidcud <51078365+peidcud@users.noreply.github.com>
2020-08-08 09:43:21 +0800

    Completed create users portion of website

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -2,11 +2,10 @@ from django import forms
 from django.forms import ModelForm
 from .models import NewsStory
 
-
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'author', 'pub_date', 'image_url', 'content']
+        fields = ['title', 'pub_date', 'content', 'image_url']
         css = {
             'all': ('styles.css')
         }
--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -2,14 +2,17 @@ from django.db import models
 # For ordering news articles by date
 from django.db.models import DateTimeField
 from django.db.models.functions import Trunc
+from django.contrib.auth import get_user_model
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
     pub_date = models.DateTimeField()
     content = models.TextField()
-    image_url = models.URLField(max_length=400)
+    image_url = models.URLField()
     # gets rid of time stamp on entry
     # def date_published(self):
     #     return self.pub_date.strftime('%B %d %Y')
-
